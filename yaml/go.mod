c6a9e6615f9f4c1d924fb1a39a9c87c47fced0fb
Akihiko Ito <hi.mound@gmail.com>
2021-01-23 12:28:13 +0900

    add yaml

---
+++ yaml/go.mod
@@ -0,0 +1,8 @@
+module example.com/yaml-test
+
+go 1.15
+
+require (
+	github.com/kr/pretty v0.2.1
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ yaml/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	yaml "gopkg.in/yaml.v2"
+	"fmt"
+  pretty "github.com/kr/pretty"
+)
+
+const conf = "./config.yaml"
+
+func main() {
+
+  configbuf, err := ioutil.ReadFile(conf)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  //data, err := ReadOnSliceMap(configbuf)
+  yamldata := make(map[interface{}]interface{})
+  err = yaml.Unmarshal(configbuf, &yamldata)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+	pretty.Print("--- yamldata:\n%# v\n\n", yamldata)
+
+	fmt.Println()
+	fmt.Println(yamldata["hoge"].(string))
+	roles := []string{}
+  for _, value := range yamldata["roles"].([]interface {}) {
+		roles = append(roles, value.(string))
+	}
+	fmt.Println(yamldata["roles"])
+	fmt.Println(roles)
+
+}
